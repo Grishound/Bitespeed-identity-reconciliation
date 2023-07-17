d1a35d88d36df16849e0d7e24855acf4c2d249ed
Mayank Parashar <mayankparashar2000@gmail.com>
2023-07-17 16:48:52 +0530

    Coonected to render database.

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,16 @@
+import os
 from flask import Flask
 from .models import db
 from .functions import get_existing_primary_contacts, return_response
 from flask_json import FlaskJSON
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@localhost/database_name'
+
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+
+#render database
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+
 db.init_app(app)
 FlaskJSON(app)
 
